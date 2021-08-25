4fcddd28443a4a99e7c3ed2642ad7dfda72691b1
andreallata <andrea.alt92@gmail.com>
2021-08-25 12:33:58 -0400

    tech stach

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -13,18 +13,18 @@ const Home = (props) => {
       </h2>
 
       <section className="icons">
-        <img src="https://imgur.com/TtT9ots.png" alt="HTML"/>
-        <img src="https://imgur.com/xYB7zh4.png" alt="css" />
-        <img src="https://imgur.com/XxPMXZa.png" alt="JavaScript"/>
-        <img src="https://imgur.com/O2SGzz2.png" alt="react"/>
-        <img src="https://imgur.com/pfnHrUy.png" alt="express"/>
-        <img src="https://imgur.com/e8ksfgN.png" alt="node"/>
-        <img src="https://imgur.com/K6DsHSc.png" alt="git"/>
-        <img src="https://imgur.com/Hs0zRyY.png" alt="mongoDB"/>
-        <img src="https://imgur.com/t48aukM.png" alt="sql"/>
-        <img src="https://imgur.com/iT58yjW.png" alt="postgresql"/>
-        <img src="https://imgur.com/E4np4Cx.png" alt="rails" />
-        <img src="https://imgur.com/t6kPJZb.png" alt="ruby"/>
+        <img className="icon" src="https://imgur.com/6bbdJXw.png" alt="HTML"/>
+        <img className="icon" src="https://imgur.com/12g4gdD.png" alt="css" />
+        <img className="icon" src="https://imgur.com/XxPMXZa.png" alt="JavaScript"/>
+        <img className="icon" src="https://imgur.com/O2SGzz2.png" alt="react"/>
+        <img className="icon"  src="https://imgur.com/740cn0P.png" alt="express"/>
+        <img className="icon" src="https://imgur.com/HDvOP3a.png" alt="node"/>
+        <img className="icon" src="https://imgur.com/8S73zZR.png" alt="git"/>
+        <img className="icon" src="https://imgur.com/kP9zhYk.png" alt="mongoDB"/>
+        <img className="icon" src="https://imgur.com/sScVgck.png" alt="sql"/>
+        <img className="icon"src="https://imgur.com/vN9eNg0.png" alt="postgresql"/>
+        <img className="icon" src="https://imgur.com/jWtiD2G.png" alt="rails" />
+        <img className="icon" src="https://imgur.com/I7NRqX2.png" alt="ruby"/>
       </section>
 
       <Link to="/developer_projects">
