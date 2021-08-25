b6d7b18e0bd0186a89614e5ef92e6555d263ec4a
andreallata <andrea.alt92@gmail.com>
2021-08-25 12:53:08 -0400

    tech stach

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -15,6 +15,7 @@ const NavBar = () => {
       <div className="nav">
       <NavLink className="vd-link" to="/design_projects">Visual Designer</NavLink>
       <NavLink className="fd-link" to="/developer_projects">Full-Stack Developer</NavLink>
+      <a className="r-link" href="https://drive.google.com/file/d/1O31zgsx6n_gBY6nEKZyEosiQUpfnCnHA/view?usp=sharing">Resumé</a>
        </div>
     </nav>
   );
